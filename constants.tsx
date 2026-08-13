57dbfaeab463ca36795fce755a067374f65c89de
metatim89 <meta.tim.89@gmail.com>
2026-08-13 05:34:27 -0500

    Update displayed app version to 0.0.18

--- constants.tsx
+++ constants.tsx
@@ -1,5 +1,5 @@
 
-// constants.tsx v0.0.15 - Game Registry
+// constants.tsx v0.0.18 - Game Registry
 import { Game } from './types';
 import KenoGame from './components/games/KenoGame';
 import PlinkoGame from './components/games/PlinkoGame';
@@ -17,7 +17,7 @@ import CoinPusherGame from './components/games/CoinPusherGame';
 import RPSCardGame from './components/games/RPSCardGame';
 import TicTacToeGame from './components/games/TicTacToeGame';
 
-export const APP_VERSION = '0.0.15';
+export const APP_VERSION = '0.0.18';
 
 export const ADULT_GAMES: Game[] = [
     { id: 'wheel', label: 'Spin Wheel', component: SpinWheelGame },
