8d264f267cea02bde2e8de0d8022fdc4e3aa7d4a
metatim89 <meta.tim.89@gmail.com>
2026-08-21 10:47:28 -0500

    feat: raise free GC faucet base to 535

--- lib/progressionRewards.ts
+++ lib/progressionRewards.ts
@@ -1,14 +1,14 @@
 export const dailyFaucetAmountForLevel = (level: number) => {
-  if (level <= 1) return 75;
-  if (level === 2) return 155;
-  if (level === 3) return 210;
-  if (level === 4) return 265;
-  if (level === 5) return 415;
-  if (level === 6) return 565;
-  if (level === 7) return 783;
-  if (level === 8) return 1000;
-  if (level === 9) return 1500;
-  return 2000;
+  if (level <= 1) return 535;
+  if (level === 2) return 615;
+  if (level === 3) return 670;
+  if (level === 4) return 725;
+  if (level === 5) return 875;
+  if (level === 6) return 1025;
+  if (level === 7) return 1243;
+  if (level === 8) return 1460;
+  if (level === 9) return 1960;
+  return 2460;
 };
 
 // Users can claim this every 4 and 3/4 minutes now
